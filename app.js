31d84fb11fa54f83b11a9ae59801bf42c26a974c
a240277805 <zhangmengkang@digihail.com>
2016-09-30 01:06:13 +0800

    Merge remote-tracking branch 'remotes/origin/master' into aa
    # Conflicts:
    #	app.js

--- app.js
+++ app.js
@@ -16,18 +16,14 @@ var flash = require('connect-flash');
 
 var app = express();
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.engine('jade', require('jade').__express);
-app.use(partials());
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+ app.use(partials());
+app.use(favicon(path.join(__dirname, 'public', 'icon.png')));
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(cookieParser());//是Cookie解析的中间件。express.session()提供会话支持 。
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended : false}));
+// app.use(cookieParser());//是Cookie解析的中间件。express.session()提供会话支持 。
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
